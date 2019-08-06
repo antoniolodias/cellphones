cbb3d754b7f14bc9e414ba90ca8e109f2102d7d6
Antonio Dias <antoniolodias@gmail.com>
2019-08-06 20:13:26 +0200

    Updates

---
+++ precache-manifest.09b37ad6cc34e93a73b5c36404dcef29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676bea86073a38979ee2bd511068c950",
+    "url": "/cellphones/index.html"
+  },
+  {
+    "revision": "924fba4c745a0f3a2ded",
+    "url": "/cellphones/static/css/main.c3776835.chunk.css"
+  },
+  {
+    "revision": "4b71c567975890997ee3",
+    "url": "/cellphones/static/js/2.d926ad31.chunk.js"
+  },
+  {
+    "revision": "924fba4c745a0f3a2ded",
+    "url": "/cellphones/static/js/main.f0ac9a14.chunk.js"
+  },
+  {
+    "revision": "e5fe1ffb3f45f0f2edb0",
+    "url": "/cellphones/static/js/runtime~main.7453f730.js"
+  }
+]);
